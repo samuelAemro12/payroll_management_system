4c6a3e144122d266a06b47e437c897c78b48d633
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-24 12:13:06 -0700

    added the intro part for the CLI app

--- main.cpp
+++ main.cpp
@@ -5,3 +5,12 @@ int main (){
  cout<<"hello world"<<endl;
 return 0;
 }
+
+void intro() {
+    cout << "\t\tUNIVERSITY OF GONDAR\n\n";
+    cout << "Atse Tewodros Finance Office\n";
+    cout << "Payroll Management System\n";
+    cout << "CREATED BY:-\n";
+    cout << "SAMUEL AEMRO and LEUL YEWONDWOSSEN\n\n";
+    // system("cls"); // Optional
+}
