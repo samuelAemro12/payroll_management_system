35d82aefdf887a9d22cb3653ac36b06aaaad773c
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-24 12:26:10 -0700

    added the menu function for user navigation menu

--- main.cpp
+++ main.cpp
@@ -15,3 +15,27 @@ void intro() {
     cout << "SAMUEL AEMRO and LEUL YEWONDWOSSEN\n\n";
     // system("cls"); // Optional
 }
+void menu() {
+    int choice;
+
+    do {
+        cout << "\n======= MENU ==========\n";
+        cout << "1. Enter Employee Info and Calculate Payroll\n";
+        cout << "2. Exit\n";
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                input();
+                break;
+            case 2:
+                cout << "\nThank you and Farewell!\n";
+                break;
+            default:
+                cout << "\nInvalid option. Please try again.\n";
+                // system("cls"); // Optional for Windows users
+        }
+
+    } while (choice != 2);
+}
